5e9b96138b3a216f5314b8d548929255cd6f5de2
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2021-01-13 21:52:52 +0800

    Update App.vue and File Landing Page

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="LandingPage">
+    <Navbar />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/NavigationBar.vue'
+
+export default {
+  name: 'LandingPage',
+  components: {
+    Navbar
+  }
+}
+</script>
