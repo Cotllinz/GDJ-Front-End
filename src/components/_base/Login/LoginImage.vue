916bbf7bbd1fd6cd24627d97ccad021b3ef93fd2
andrawan <andrawan.ep@gmail.com>
2021-01-13 22:58:21 +0700

    page login pekerja

---
+++ src/components/_base/Login/LoginImage.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="main">
+    <div id="wrapper">
+      <div class="image">
+        <div class="text1">
+          <img src="@/assets/logo.png" />
+          Get Dream Job
+        </div>
+        <div class="text2">
+          Temukan developer berbakat & terbaik di berbagai bidang keahlian
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginImage'
+}
+</script>
+
+<style scoped>
+#main {
+  padding: 50px;
+  padding-top: 35px;
+}
+#wrapper {
+  background-image: url('../../../assets/login-image.png');
+  height: 820px;
+  overflow: hidden;
+  color: #ffffff;
+}
+.image {
+  background: rgba(8, 107, 199, 0.726);
+  overflow: hidden;
+  height: 100%;
+  z-index: 2;
+}
+.text1 {
+  padding-top: 50px;
+  padding-left: 50px;
+  font-family: sans-serif;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 19px;
+}
+.text2 {
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 44px;
+  line-height: 70px;
+  padding: 70px;
+  padding-top: 190px;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="login">
+    <b-container fluid>
+      <b-row>
+        <b-col><LoginImage /></b-col>
+        <b-col><LoginComponent /></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LoginComponent from '../components/_base/Login/LoginComponent'
+import LoginImage from '../components/_base/Login/LoginImage'
+
+export default {
+  name: 'Login',
+  components: {
+    LoginImage,
+    LoginComponent
+  }
+}
+</script>
+
+<style scoped>
+.login {
+  background-color: rgb(250, 245, 245);
+}
+</style>
