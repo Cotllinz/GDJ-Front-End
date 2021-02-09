092f37437f3a3d7561dfab3eddc51f96f9ca6526
andrawan <andrawan.ep@gmail.com>
2021-02-09 10:04:39 +0700

    chat room integrated

--- src/components/_base/ChatRoom/ChatConversation.vue
+++ src/components/_base/ChatRoom/ChatConversation.vue
@@ -1,44 +1,103 @@
 <template>
   <div>
-    <b-card class="card-style p-0">
-      <h6 class="title space-card">Firman Azhar R</h6>
-      <hr />
-      <div class="space-card">
-        <div class="left-conversation">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident at
-          eaque ex. Quas tenetur, alias,
+    <div v-if="chatMode === true">
+      <b-card class="card-style p-0">
+        <h6 class="title space-card">{{ chatActive.username }}</h6>
+        <hr />
+        <div class="space-card">
+          <div class="left-conversation">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            at eaque ex. Quas tenetur, alias,
+          </div>
+          <div class="right-conversation">
+            sint vel officiis atque
+          </div>
         </div>
-        <div class="right-conversation">
-          sint vel officiis atque
+        <div class="enter-chat space-chat centered">
+          <b-form-input
+            type="text"
+            class="input-chat"
+            placeholder="Write your message here ..."
+          ></b-form-input>
+          <div style="font-size: 35px;">
+            <b-icon
+              icon="cursor-fill"
+              class="rounded-circle p-2"
+              style="background-color:#5E50A1"
+              variant="light"
+            ></b-icon>
+          </div>
         </div>
-      </div>
-      <div class="enter-chat space-chat centered">
-        <b-form-input
-          type="text"
-          class="input-chat"
-          placeholder="Write your message here ..."
-        ></b-form-input>
-        <div style="font-size: 35px;">
-          <b-icon
-            icon="cursor-fill"
-            class="rounded-circle p-2"
-            style="background-color:#5E50A1"
-            variant="light"
-          ></b-icon>
+      </b-card>
+    </div>
+    <div v-else>
+      <b-card class="card-style p-0">
+        <h6 class="title space-card"></h6>
+        <hr />
+        <div
+          style="display:flex;align-items:center;justify-content:center;margin-top:200px"
+        >
+          Pilih Chat untuk memulai
         </div>
-      </div>
-    </b-card>
+        <div class="space-card"></div>
+      </b-card>
+    </div>
   </div>
 </template>
 <script>
+import { mapGetters, mapActions } from 'vuex'
+import io from 'socket.io-client'
 export default {
   name: 'ChatConversation',
   data() {
     return {
-      chat: [1, 2, 3]
+      chat: 1,
+      socket: io(`http://${process.env.VUE_APP_PORT}`, {
+        withCredentials: true,
+        extraHeaders: {
+          'terbangin-header': 'ini terbangin header'
+        }
+      }),
+      message: '',
+      URL: `http://${process.env.VUE_APP_URL}`
     }
   },
-  methods: {}
+  created() {
+    this.getUserProfile(this.user.userId)
+  },
+  computed: {
+    ...mapGetters({
+      user: 'setUser',
+      chatMode: 'getterChatMode',
+      chatActive: 'getterChatActive',
+      activeRoom: 'getterActiveRoom',
+      messages: 'getterMessages',
+      messagesHistory: 'getterMessagesHistory',
+      profile: 'setProfile'
+    })
+  },
+  methods: {
+    ...mapActions(['sendMessages', 'getChatRoom', 'getUserProfile']),
+    sendMessage() {
+      const setData = {
+        sender: this.user.userId,
+        message: this.message,
+        room: this.chatActive.roomIdUniq,
+        senderPic: this.profile.profileImage,
+        receiverPic: this.chatActive.profileImage
+      }
+      this.socket.emit('roomMessage', setData)
+      const dataMessage = {
+        roomIdUniq: this.chatActive.roomIdUniq,
+        sender: this.user.userId,
+        receiver: this.chatActive.userId,
+        message: this.message
+      }
+      this.sendMessages(dataMessage)
+      this.message = ''
+      this.getChatRoom(this.user.userId)
+    }
+  }
 }
 </script>
 
